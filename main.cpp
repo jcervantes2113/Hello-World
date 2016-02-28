657f145141c9d4b85e6562924d4f4791ca6d769a
jcervantes2113 <josecervantes678@gmail.com>
2016-02-27 21:36:55 -0800

    Hello World

---
+++ main.cpp
@@ -0,0 +1,36 @@
+/* 
+ * File:   main.cpp
+ * Author: Jose L Cervantes
+ *
+ * Created on February 17, 2016, 9:45 AM
+ * Purpose: Our first program 
+ */
+//system Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function  Prototypes
+
+//Execution Begins Here!
+
+int main(int argc, char** argv) {
+// Declare Variables 
+    
+//Initialize Variables
+    
+//Input Values 
+    
+//Map The Inputs to the Outputs
+    
+//Output the Results
+    
+cout<<"Hello World"<<endl;
+
+//Exit Stage Right!
+    return 0;
+}
+
